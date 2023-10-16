0d7d3494fe5591f54b57aa54f90a9130363abb11
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-10-16 10:53:24 +0200

    7 - [Homepage] Cards

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -69,7 +69,8 @@ function detectSidebar(main) {
 
     if (sidebarOffset && Number.parseInt(sidebar.getAttribute('data-start-sidebar-at-section'), 10)) {
       const offset = Number.parseInt(sidebar.getAttribute('data-start-sidebar-at-section'), 10);
-      sidebar.style = `grid-row: ${offset} / infinite;`;
+      sidebar.style.gridRow = `${offset} / infinite`;
+      console.log(sidebar.style);
     }
 
     sidebar.querySelectorAll('h3').forEach((header) => {
