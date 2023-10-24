1fe03dab8a6d5fa3a8113d880df5c15fc0d77950
suazad <suazad@adobe.com>
2023-10-24 15:48:23 +0200

    Changing path of footer/;

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -10,7 +10,7 @@ export default async function decorate(block) {
 
   // fetch footer content
   const footerPath = cfg.footer || '/footer';
-  const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/drafts/suazad/footer') ? { cache: 'reload' } : {});
+  const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
 
   if (resp.ok) {
     const html = await resp.text();
@@ -19,7 +19,7 @@ export default async function decorate(block) {
     const footer = document.createElement('div');
     footer.innerHTML = html;
 
-    footer.children[0].classList.add('contact');
+    footer.children[0].classList.addClass('contact');
     footer.children[1].classList.add('company-links');
     footer.children[2].classList.add('socials');
     footer.children[3].classList.add('lower-footer');
