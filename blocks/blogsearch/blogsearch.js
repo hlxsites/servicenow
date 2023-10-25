c7f464a51d39a40b03e56513e76044ef4e03b5b7
Ben Peter <benp@adobe.com>
2023-10-25 13:58:32 +0200

    more style

--- blocks/blogsearch/blogsearch.js
+++ blocks/blogsearch/blogsearch.js
@@ -3,13 +3,8 @@ import {
 } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(block) {
-  console.log('1');
-  console.log(block.innerHtml);
   block.innerHtml = '';
   block.textContent = '';
-  console.log('2');
-  console.log(block.innerHtml);
-  console.log('decorating blogsearch');
   block.append(
     form({},
       div({},
