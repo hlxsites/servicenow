8442b93d160768d7de8b45fb2509ecbb729704f1
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-10-31 18:14:27 +0100

    38 - Integrate Adobe DTM

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './aem.js';
+import { loadScript, sampleRUM } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+loadScript('https://assets.adobedtm.com/a441b904b50e/7a4facbbcffb/launch-039be8795dc8.min.js', { async: "" });
