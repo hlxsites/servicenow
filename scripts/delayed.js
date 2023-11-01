970100e81bfc62418786a0924faba37baaef2412
Dereje Dilnesaw <dilnesaw@adobe.com>
2023-11-01 13:20:19 -0700

    added scripts for social share on blogs

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './aem.js';
+import { loadScript, sampleRUM } from './aem.js';
 
+loadScript('https://buttons-config.sharethis.com/js/5cae6f10a8698b001266ffd9.js');
+loadScript('https://platform-api.sharethis.com/js/sharethis.js#property=5cae6f10a8698b001266ffd9&product=inline-share-buttons');
+loadScript('https://count-server.sharethis.com/v2.0/get_counts?cb=window.__sharethis__.cb&url=https%3A%2F%2Fwww.servicenow.com%2Fblogs%2F2023%2Fvancouver-release-genai-security-agility.html');
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
