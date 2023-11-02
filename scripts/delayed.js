4c75a4dea6b8a06187f0f6f503346ba27a8f55b5
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-11-02 20:34:03 +0100

    38 - Integrate Adobe DTM

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,6 +5,16 @@ import { loadScript, sampleRUM } from './aem.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
-loadScript(`${window.hlx.codeBasePath}/scripts/jquery-3.7.1.min.js`).then(() => {
-  loadScript('https://assets.adobedtm.com/a441b904b50e/7a4facbbcffb/launch-039be8795dc8.min.js', { async: '' });
-});
+async function loadAdobeDTM() {
+  await loadScript(`${window.hlx.codeBasePath}/scripts/jquery-3.7.1.min.js`);
+  const prod = 'https://assets.adobedtm.com/a441b904b50e/7a4facbbcffb/launch-039be8795dc8.min.js';
+  const stage = 'https://assets.adobedtm.com/a441b904b50e/7a4facbbcffb/launch-a2ae4c3b0523-staging.min.js';
+
+  if (window.location.host.endsWith('.page') || window.location.host.startsWith('localhost')) {
+    loadScript(stage, { async: '' });
+  } else {
+    loadScript(prod, { async: '' });
+  }
+}
+
+loadAdobeDTM();
