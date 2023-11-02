56c152f94ab43b064410a2b51cb78e4c0cc9df7a
Ben Peter <benp@adobe.com>
2023-11-02 09:10:25 +0100

    hard-code path check if response ok

--- blocks/article-copyright/article-copyright.js
+++ blocks/article-copyright/article-copyright.js
@@ -2,20 +2,19 @@ import { getMetadata } from '../../scripts/aem.js';
 import { getLocaleInfo } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
-  const copyrightPath = getMetadata('copyright-path');
-
   const localeInfo = getLocaleInfo();
-  const copyrightFragmentPath = copyrightPath
-    ? new URL(copyrightPath).pathname
-    : 'article-copyright';
 
-  const copyrightPromise = fetch(`${localeInfo.urlPrefix}/blogs/fragments/${copyrightFragmentPath}.plain.html`);
+  const copyrightPromise = fetch(`${localeInfo.urlPrefix}/blogs/fragments/article-copyright.plain.html`);
 
   const publicationDate = getMetadata('publication-date');
   const d = new Date(publicationDate);
   const year = d.toLocaleDateString('en', { year: 'numeric' });
 
   const copyrightFragment = await copyrightPromise;
+  if (!copyrightFragment.ok) {
+    block.innerHTML = `<div><p>&copy; ${year} ServiceNow</p></div>`;
+    return;
+  }
   const copyrightText = (await copyrightFragment.text()).replace('#year#', year);
   block.innerHTML = copyrightText;
 }
