3ec7c8452eb6f8101daadc53019a2212ec65a107
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-11-03 10:39:07 +0100

    38 - Integrate Adobe DTM (#61)

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,21 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './aem.js';
+import { loadScript, sampleRUM } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+async function loadAdobeDTM() {
+  await loadScript(`${window.hlx.codeBasePath}/scripts/jquery-3.7.1.min.js`);
+  const prod = 'https://assets.adobedtm.com/a441b904b50e/7a4facbbcffb/launch-039be8795dc8.min.js';
+  const stage = 'https://assets.adobedtm.com/a441b904b50e/7a4facbbcffb/launch-a2ae4c3b0523-staging.min.js';
+
+  const { host } = window.location;
+  if (host === 'servicenow.com' || host === 'www.servicenow.com' || host.endsWith('.live')) {
+    loadScript(prod, { async: '' });
+  } else {
+    loadScript(stage, { async: '' });
+  }
+}
+
+loadAdobeDTM();
