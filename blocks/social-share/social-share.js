264c43e16906937e18536c87ab30058a828d3f25
Dereje Dilnesaw <dilnesaw@adobe.com>
2023-11-06 08:07:07 -0800

    fix js lint issues

--- blocks/social-share/social-share.js
+++ blocks/social-share/social-share.js
@@ -1,36 +1,22 @@
 import { loadScript } from '../../scripts/aem.js';
-import { getLocaleInfo } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
+  block.classList.add('sharethis-inline-share-buttons');
 
-async function loadSocialShare() {
-  loadScript('https://platform-api.sharethis.com/js/sharethis.js#property=5cae6f10a8698b001266ffd9&product=inline-share-buttons');
+  async function loadSocialShare() {
+    loadScript('https://platform-api.sharethis.com/js/sharethis.js#property=5cae6f10a8698b001266ffd9&product=inline-share-buttons');
+  }
 
-    const localeInfo = getLocaleInfo();
-
-    const socialSharePromise = fetch(`${localeInfo.urlPrefix}/blogs/fragments/social-share.plain.html`);
-
-    const socialShareFragment = await socialSharePromise;
-    if (!socialShareFragment.ok) {
-        block.innerHTML = `<div></div>`;
-        return;
+  const observer = new IntersectionObserver((entries) => {
+    if (entries.some((entry) => entry.isIntersecting)) {
+      observer.disconnect();
+      loadSocialShare();
     }
-}
+  });
+  observer.observe(block);
 
-const socialShareContainer = block.querySelector('.sharethis-inline-share-buttons');
-if (socialShareContainer) {
-    const observer = new IntersectionObserver((entries) => {
-        if (entries.some((entry) => entry.isIntersecting)) {
-            observer.disconnect();
-            loadSocialShare(socialShareContainer);
-        }
-    });
-    observer.observe(socialShareContainer);
-
-    setTimeout(() => {
-        observer.disconnect();
-        loadSocialShare(socialShareContainer);
-    }, 3000);
-
-}
+  setTimeout(() => {
+    observer.disconnect();
+    loadSocialShare();
+  }, 3000);
 }
