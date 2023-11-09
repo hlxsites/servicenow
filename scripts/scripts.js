0a976b6d93565ef83190cb9d4ef64020aaf89b43
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-11-09 22:06:04 +0100

    test - delay more

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -309,7 +309,7 @@ async function loadLazy(doc) {
  */
 function loadDelayed() {
   // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 4000);
+  window.setTimeout(() => import('./delayed.js'), 4500);
   // load anything that can be postponed to the latest here
 }
 
