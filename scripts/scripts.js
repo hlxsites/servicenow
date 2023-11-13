a33acfbe65d386d5b3d82cdefe79fef91adbef90
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-11-13 10:18:25 +0100

    test - Optimise NaaS LHS - delay more

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -310,7 +310,7 @@ async function loadLazy(doc) {
  */
 function loadDelayed() {
   // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 3000);
+  window.setTimeout(() => import('./delayed.js'), 3500);
   // load anything that can be postponed to the latest here
 }
 
