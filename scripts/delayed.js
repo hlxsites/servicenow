afd467394f0cb06b1187285d3071f48efd2d99cc
Ben Peter <benp@adobe.com>
2023-11-14 15:06:00 +0100

    fix #76

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,6 +5,9 @@ import { loadScript, sampleRUM } from './aem.js';
 sampleRUM('cwv');
 // add more delayed functionality here
 async function loadAdobeDTM() {
+  if (new URLSearchParams(window.location.search).get('disableLaunch') === 'true') {
+    return;
+  }
   await loadScript(`${window.hlx.codeBasePath}/scripts/jquery-3.7.1.min.js`);
   const prod = 'https://assets.adobedtm.com/a441b904b50e/7a4facbbcffb/launch-039be8795dc8.min.js';
   const stage = 'https://assets.adobedtm.com/a441b904b50e/7a4facbbcffb/launch-a2ae4c3b0523-staging.min.js';
