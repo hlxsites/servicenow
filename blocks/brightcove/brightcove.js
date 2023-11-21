ead61036f747e052fea35155c07ef9cfd6fbbc0b
suazad <suazad@adobe.com>
2023-11-21 15:23:51 +0100

    Ading brightcove block

---
+++ blocks/brightcove/brightcove.js
@@ -0,0 +1,46 @@
+import { readBlockConfig } from '../../scripts/aem.js';
+
+export default async function decorate(block) {
+  const config = readBlockConfig(block);
+
+  console.log('Brightcove video config', JSON.stringify(config));
+  const picture = block.querySelector('picture');
+  const head = document.querySelector('head');
+
+  let account = config.account ? config.account : undefined;
+  let player = config.player ? config.player : undefined;
+
+  if (!account || !player) {
+    // eslint-disable-next-line no-console
+    console.log('Brightcove video not configured.');
+    block.innerHTML = '';
+    return;
+  }
+
+  block.innerHTML = `
+    <div class="placeholder">
+        ${picture.outerHTML}
+        <button class="play-button" type="button" title="Play Video" aria-disabled="false">
+          <span class="arrow" aria-hidden="true"></span>
+          <span class="sr-only" aria-live="polite"></span>
+        </button>
+    </div>
+    <div class="video">
+      <video-js autoplay data-video-id="${config.video}" data-account="${account}" data-player="${player}" controls="" data-embed="default" aria-label="Video Player"></video-js>
+    </div>
+  `;
+
+  const placeholder = block.querySelector('.placeholder');
+
+  const loadBrightcovePlayer = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    const script = document.createElement('script');
+    script.setAttribute('src', `https://players.brightcove.net/${account}/${player}_default/index.min.js`);
+    head.append(script);
+    block.classList.add('play-mode');
+    placeholder.removeEventListener('click', loadBrightcovePlayer);
+  };
+
+  placeholder.addEventListener('click', loadBrightcovePlayer);
+}
