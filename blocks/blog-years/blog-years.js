0b51e601bd14c3345e65d931a63e3689cfacd769
VenkatKrishnaSN <venkatkrishna.tammin@servicenow.com>
2023-12-07 17:49:11 +0530

    Updated the logic to display first 4 items and filter by locale
    Updated the logic to display first 4 items and filter by locale

--- blocks/blog-years/blog-years.js
+++ blocks/blog-years/blog-years.js
@@ -1,9 +1,11 @@
-import { fetchAPI } from '../../scripts/scripts.js';
+import { fetchAPI, getLocaleInfo } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
   const responseYears = await fetchAPI('/blogs/query-index.json?sheet=years');
+  const localeInfo = getLocaleInfo();
   const blogYears = responseYears.data;
-  blogYears.forEach((item) => {
+  const filteredBlogYears = blogYears.filter((item) => item.locale === localeInfo);
+  filteredBlogYears.slice(0, 4).forEach((item) => {
     const pTag = document.createElement('p');
     pTag.setAttribute('class', 'button-container');
     const aTag = document.createElement('a');
