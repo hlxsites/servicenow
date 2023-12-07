4c129157b87d8eaca3f7f049469c11484b31ebda
VenkatKrishnaSN <108878839+VenkatKrishnaSN@users.noreply.github.com>
2023-12-07 20:32:15 +0530

    Issue49 topicyears (#174)

---
+++ blocks/blog-years/blog-years.js
@@ -0,0 +1,19 @@
+import { fetchAPI, getLocale } from '../../scripts/scripts.js';
+
+export default async function decorate(block) {
+  const responseYears = await fetchAPI('/blogs/query-index.json?sheet=years');
+  const localeInfo = getLocale();
+  const blogYears = responseYears.data;
+  const filteredBlogYears = blogYears.filter((item) => item.locale === localeInfo);
+  filteredBlogYears.slice(0, 4).forEach((item) => {
+    const pTag = document.createElement('p');
+    pTag.setAttribute('class', 'button-container');
+    const aTag = document.createElement('a');
+    aTag.setAttribute('href', item.path);
+    aTag.setAttribute('title', item.title);
+    const textNode = document.createTextNode(item.header);
+    aTag.appendChild(textNode);
+    pTag.appendChild(aTag);
+    block.appendChild(pTag);
+  });
+}
