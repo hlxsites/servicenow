b8b3aafcb6418ff9b452be96515eefab028acc6a
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-12-13 21:12:37 +0200

    fix - naas styling and images

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -48,6 +48,10 @@ export default async function decorate(block) {
     // trigger and wait for NaaS header rendering
     await new Promise((resolve) => {
       document.addEventListener('nass-header-rendered', () => {
+        if (!window.location.host.endsWith('servicenow.com')) {
+          document.querySelectorAll('header img[src^="/content/dam"]')
+            .forEach((image) => { image.src = `https://www.servicenow.com${new URL(image.src).pathname}`; });
+        }
         resolve();
       });
 
