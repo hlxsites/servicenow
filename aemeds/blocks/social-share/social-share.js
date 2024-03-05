b89cadc79f0799b85ffaa01de904bc5b5adb1a9d
Andrei Tuicu <andrei.tuicu@gmail.com>
2024-03-05 10:59:43 +0100

    206 - Adjust GoLive date into the social share component (#211)

--- aemeds/blocks/social-share/social-share.js
+++ aemeds/blocks/social-share/social-share.js
@@ -6,7 +6,7 @@ import {
 } from '../../scripts/scripts.js';
 
 // FIXME: update date before launch
-const esdCutOff = new Date('2024-01-08'); // 8 January 2024
+const esdCutOff = new Date('2024-03-30'); // 30 March 2024
 
 function socialShareTracking(block) {
   block.addEventListener('click', (e) => {
